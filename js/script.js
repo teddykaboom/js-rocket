6980f06ec3630a04fdc17c2aa33358651ac5ee07
Teddy <Edward@mr9999.ca>
2018-10-11 19:59:42 -0600

    updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+		// hey this is invisible comment
+		
+		var doCoolStuff = function() {
+			var currentClassName = document.getElementById('cool').className;
+			if (currentClassName == 'cool') {
+				document.getElementById('cool').className='cool red';
+			} else {
+				document.getElementById('cool').className='cool';
+			}
+		}
+		
 		var car = {
 			make: 'VW',
 			type: 'polo',
